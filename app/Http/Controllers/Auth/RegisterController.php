e1527e47c68f311dbce8490114cb71cdd4d1cfc1
oussama <oussama.ennadafy@gmail.com>
2022-04-14 19:56:24 +0000

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\controller;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -10,5 +10,20 @@ class RegisterController extends Controller
     public function index()
     {
         return view('auth.register');
+    }
+    public function store(Request $request)
+    {
+         $this->validate($request , [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|max:255',
+            'password' => 'required|confirmed',
+        ]);
+        dd('store');
+
+        //validation 
+        //store user
+        //sign the user in
+        //redirect
     }
 }
