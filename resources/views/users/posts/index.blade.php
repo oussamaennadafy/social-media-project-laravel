42b95fc9e45741763952e407f0567e9aaeaca001
oussama <oussama.ennadafy@gmail.com>
2022-05-16 16:52:16 +0100

    stable upfate

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -2,14 +2,27 @@
 
 @section('content')
     <div class="flex justify-center">
-     <div class="w-8/12 bg-white p-6 rounded-lg">
-      @if ($posts->count())
-      @foreach ($posts as $post)
-        <x-post :post='$post' />
-       @endforeach
-       @else
-       <p>{{ $user->name}} does not have any posts</p>
-       @endif
+     <div class="w-8/12">
+
+      
+      <div class="p-6 bg-gray-100 rounded-lg mb-4 w-fit">
+      <h1 class="text-2xl font-medium mb-1">{{ $user->name }}</h1>
+      <p>Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }}</p>
      </div>
-    </div>
+     
+     
+     
+     
+     <div class="w-8/12 bg-white p-6 rounded-lg">
+       @if ($posts->count())
+       @foreach ($posts as $post)
+       <x-post :post='$post' />
+       @endforeach
+        @else
+        <p>{{ $user->name}} does not have any posts</p>
+        @endif
+       </div>
+       
+      </div>
+      </div>
 @endsection
