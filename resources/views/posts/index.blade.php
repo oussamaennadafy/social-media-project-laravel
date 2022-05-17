0e0fc30f058b293f08ad1df6a348a3ecdb12b769
oussama <oussama.ennadafy@gmail.com>
2022-05-17 11:53:26 +0100

    update

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
- <div class="flex justify-center">
-  <div class="w-8/12 bg-white p-6 rounded-lg mb-10">
+ <div class="flex justify-between flex-col">
+  <div class="w-8/12 bg-white p-6 rounded-lg mb-10 mx-auto">
    <form action="{{route('posts.form')}}" method="POST" class="mb-4">
     @csrf
    <div class="mb-4">
@@ -19,7 +19,10 @@
    <div>
        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">post</button>
    </div>
+   </div>
    </form>
+   <div class="w-8/12 bg-white p-6 rounded-lg mb-10 mx-auto">
+    <h2 class="text-2xl font-bold text black p-6">What’s New</h2>
       @if ($posts->count())
       @foreach ($posts as $post)
       <div class="border-2 border-gray-100 bg-gray-50 p-6 mb-3 rounded-lg">
@@ -30,6 +33,7 @@
        there is no posts
        @endif
 
+  </div>
   </div>
  </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/all-posts', [PostController::class, 'index']);
+Route::post('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.form'); 
 Route::post('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy'); 
 
